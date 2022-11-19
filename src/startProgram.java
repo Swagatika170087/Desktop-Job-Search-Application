8f61612282706c5b9bef1d968e6dfa1717108458
Chi Li <clii0135@student.monash.edu>
2022-11-20 00:47:28 +1100

    changed project folders

---
+++ src/startProgram.java
@@ -0,0 +1,8 @@
+public class startProgram {
+    public static void main(String[] args) {
+//        TestDBConnection01 testDBConnection01 = new TestDBConnection01();
+//        testDBConnection01.runTest();
+        TestDBConnection testDBConnection = new TestDBConnection();
+        testDBConnection.testRun();
+    }
+}
